a42d2d30e51d13a0b44011cf2c505e3f268dc6f7
ashmit1795 <ashmitpatra77@gmail.com>
2024-10-27 00:49:26 +0530

    add server startup code to use database connection utility function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Happy developing ✨')
+import connectDB from './db/index.js';
+import express from 'express';
+import AppError from './utils/AppError.js';
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+});
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+connectDB()
+    .then(() => {
+        app.on('error', (error) => {
+            console.error(`Error: ${error.message}`);
+            throw new AppError(500, 'Server error');
+        });
+
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+    .catch((error) => {
+        console.error(`Error: ${error.message}`);
+        throw new AppError(500, 'Server error');
+    });
