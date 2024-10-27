9fcbbf9752f4d3570cc061b1b884b590674f2400
ashmit1795 <ashmitpatra77@gmail.com>
2024-10-27 20:12:31 +0530

    set the view engine to ejs.

--- src/app.js
+++ src/app.js
@@ -14,13 +14,13 @@ app.use(cors(corsOptions));
 
 // Parse incoming JSON payloads
 app.use(express.json({
-    limit: "16kb"
+    limit: '16kb'
 }));
 
 // Parse incoming urlencoded payloads
 app.use(express.urlencoded({
     extended: true,
-    limit: "16kb"
+    limit: '16kb'
 }));
 
 // To serve static files such as images, CSS files, and JavaScript files
@@ -28,3 +28,6 @@ app.use(express.static('public'));
 
 // Parse cookies from the incoming requests and populate req.cookies with an object keyed by the cookie names
 app.use(cookieParser());
+
+// Set view engine to ejs
+app.set('view engine', 'ejs');
