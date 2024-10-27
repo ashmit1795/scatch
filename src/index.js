85a23563a968d8c374ce607fdaca9164eacbd39f
ashmit1795 <ashmitpatra77@gmail.com>
2024-10-27 13:24:22 +0530

    add app import and debug namespaces for database and server

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import express from 'express';
 import AppError from './utils/AppError.js';
 import dotenv from 'dotenv';
 import debug from 'debug';
+import { app } from './app.js';
 
 // Load environment variables
 dotenv.config({
     path: './.env'
 });
 
-const app = express();
 const port = process.env.PORT || 3000;
 
+// Define a debug namespace for the database and server
 const dbDebug = debug('app:database');
 const serverDebug = debug('app:server');
 
