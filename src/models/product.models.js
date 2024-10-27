2b6a415bc189ea773897a500a5575bcbd342df09
ashmit1795 <ashmitpatra77@gmail.com>
2024-10-27 21:03:50 +0530

    add product model with required fields and default values

---
+++ src/models/product.models.js
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+// Product schema
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    discount: {
+        type: Number,
+        default: 0
+    },
+    bgColor: {
+        type: String,
+        default: "#ffffff"
+    },
+    panelColor: {
+        type: String,
+        default: "#ffffff"
+    },
+    textColor: {
+        type: String,
+        default: "#000000"
+    },
+}, {timestamps: true});
+
+// Product model
+const Product = mongoose.model("Product", productSchema);
+export default Product;
