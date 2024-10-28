4025b6326db489ff2aed4babb781dde9bf587244
ashmit1795 <ashmitpatra77@gmail.com>
2024-10-28 12:47:27 +0530

    update admin.routes.js to add routes for admin login, admin creation, and admin dashboard

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,19 +1,28 @@
-import { Router } from "express";
-import upload from "../middlewares/multer.middleware.js";
-import { createAdmin, loginAdmin } from "../controllers/admin.controllers.js"
+import { Router } from 'express';
+import upload from '../middlewares/multer.middleware.js';
+import { createAdmin, loginAdmin } from '../controllers/admin.controllers.js';
+import Admin from '../models/admin.models.js';
 
 const router = Router();
 
-router.route("/").get((req, res) => {
-    res.render("admin-login");
+router.route('/').get((req, res) => {
+        res.render('admin-login');
 }).post(loginAdmin);
 
-router.route("/create").get((req, res) => {
-    res.render("admin-create");
-}).post(upload.single("avatar"), createAdmin);
+router.route('/create').get((req, res) => {
+        res.render('admin-create');
+}).post(upload.single('avatar'), createAdmin);
 
-
-router.get("/dashboard", (req, res) => {
-    res.render("admin");
+router.route('/dashboard').get(async (req, res) => {
+    const user = await Admin.findOne({ role: 'owner' });
+    res.render('admin-dashboard', {user: user, pendingManagers: []});
 });
+
+if(process.env.NODE_ENV === 'development'){
+    router.get('/error', (req, res) => {
+        res.render('error', { message: 'Unauthorized', status: 401 });
+    });
+}
+
+
 export default router;
