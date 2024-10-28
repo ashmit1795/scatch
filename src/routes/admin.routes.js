97c4488161cef1e09aaa7435d51d8ee1bf6cc7ea
ashmit1795 <ashmitpatra77@gmail.com>
2024-10-28 12:07:09 +0530

    update admin.routes.js to add routes for admin login, admin creation, and admin dashboard

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,5 +1,19 @@
 import { Router } from "express";
+import upload from "../middlewares/multer.middleware.js";
+import { createAdmin, loginAdmin } from "../controllers/admin.controllers.js"
 
 const router = Router();
 
+router.route("/").get((req, res) => {
+    res.render("admin-login");
+}).post(loginAdmin);
+
+router.route("/create").get((req, res) => {
+    res.render("admin-create");
+}).post(upload.single("avatar"), createAdmin);
+
+
+router.get("/dashboard", (req, res) => {
+    res.render("admin");
+});
 export default router;
