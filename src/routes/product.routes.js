7a957393cb873ae510477fc50292732eeda9fe43
ashmit1795 <ashmitpatra77@gmail.com>
2024-10-29 22:52:49 +0530

    add route for creating new products

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { renderCreateProduct } from "../controllers/product.controllers.js";
 
 const router = Router();
+
+router.route('/create').get(renderCreateProduct);
 
 export default router;
