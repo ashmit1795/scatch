1744967e7ddbd3a03054c6fb04b1dce9001fbbcb
ashmit1795 <ashmitpatra77@gmail.com>
2024-10-30 19:45:41 +0530

    update product.routes.js to include authentication and authorization middleware in the create route

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import upload from '../middlewares/multer.middleware.js';
 import { createProduct, renderCreateProduct } from "../controllers/product.controllers.js";
+import { authenticateUser, authorizeUser } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route('/create').get(renderCreateProduct).post(upload.single('image'), createProduct);
+router.route('/create').get(authenticateUser, authorizeUser('owner', 'manager'), renderCreateProduct).post(authenticateUser, authorizeUser('owner', 'manager'), upload.single('image'), createProduct);
 
 export default router;
