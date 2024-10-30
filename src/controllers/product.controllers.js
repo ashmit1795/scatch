34c4795f3c1ab3445c7620f6fdf45906fab34168
ashmit1795 <ashmitpatra77@gmail.com>
2024-10-30 18:51:37 +0530

    add createProduct function to product.controllers.js

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -10,4 +10,43 @@ const renderCreateProduct = (req, res, next) => {
   res.render('create-product');
 };
 
-export { renderCreateProduct };
+const createProduct = asyncHandler(async (req, res, next) => {
+    productDebug('Creating Product');
+    const { name, description, price, category, stock, discount, bgColor, panelColor, textColor } = req.body;
+    const imageLocalPath = req.file.path;
+
+    // Upload image to Cloudinary
+    const imageUploadResponse = await uploadToCloudinary(imageLocalPath, 'product');
+    if(!imageUploadResponse.url){
+        productDebug('Image upload failed');
+        req.flash('error_msg', 'Image upload failed');
+        return res.status(500).redirect('/product/create');
+    }
+    productDebug('Image uploaded to Cloudinary');
+
+    // Create product
+    const newProduct = await Product.create({
+        name,
+        description,
+        price,
+        category,
+        stock,
+        discount,
+        bgColor,
+        panelColor,
+        textColor,
+        image: imageUploadResponse.url
+    });
+
+    const createdProduct = await Product.findById(newProduct._id);
+    if(!createdProduct){
+        productDebug('Error creating product');
+        req.flash('error_msg', 'Error creating product');
+        return res.status(500).redirect('/product/create');
+    }
+    productDebug('Product created successfully');
+    req.flash('success_msg', 'Product created successfully');
+    return res.status(201).redirect('/product/create');
+});
+
+export { renderCreateProduct, createProduct };
