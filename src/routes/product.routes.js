58a3ffc29546872fa5e2925ecfe051b58f88e795
ashmit1795 <ashmitpatra77@gmail.com>
2024-12-18 22:28:42 +0530

    add edit route for products with authentication and authorization

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import upload from '../middlewares/multer.middleware.js';
-import { createProduct, renderCreateProduct } from "../controllers/product.controllers.js";
+import { createProduct, renderCreateProduct, renderEditProduct } from "../controllers/product.controllers.js";
 import { authenticateUser, authorizeUser } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route('/create').get(authenticateUser, authorizeUser('owner', 'manager'), renderCreateProduct).post(authenticateUser, authorizeUser('owner', 'manager'), upload.single('image'), createProduct);
+router.route('/edit/:productId').get(authenticateUser, authorizeUser('owner', 'manager'), renderEditProduct);
 
 export default router;
