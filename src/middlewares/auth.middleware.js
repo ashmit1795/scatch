baba85f1b040ff1544dd88e01b1ae99f8fc0505b
ashmit1795 <ashmitpatra77@gmail.com>
2024-12-28 14:28:22 +0530

    add optional authentication middleware to handle user access token

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,6 +5,7 @@ import debug from 'debug';
 
 const authDebug = debug('app:middleware:auth');
 
+// Middleware to authenticate the user
 export const authenticateUser = asyncHandler(async (req, res, next) => {
     const accessToken = req.cookies?.accessToken || req.headers['authorization']?.split(' ')[1];
     if (!accessToken) {
@@ -30,6 +31,7 @@ export const authenticateUser = asyncHandler(async (req, res, next) => {
     next();
 });
 
+// Middleware to authorize the user based on the role
 export const authorizeUser = (...allowedRoles) => {
     return async (req, res, next) => {
         // Check if the user is authorized
@@ -49,3 +51,39 @@ export const authorizeUser = (...allowedRoles) => {
         next();
     }
 };
+
+// Middleware to authenticate the user if the access token is provided 
+export const optionalAuthenticateUser = asyncHandler(async (req, res, next) => {
+    const accessToken = req.cookies?.accessToken || req.headers['authorization']?.split(' ')[1];
+
+    if (!accessToken) {
+        // No token provided, proceed as unauthenticated
+        authDebug('No access token provided');
+        req.user = null;
+        return next();
+    }
+
+    try {
+        // Verify the access token
+        const decodedToken = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+
+        if (!decodedToken) {
+            authDebug('Invalid access token');
+            req.user = null; // Treat as unauthenticated
+            return next();
+        }
+
+        // Identify the user type based on the token
+        let user = await Admin.findById(decodedToken._id).select("-password -refreshToken");
+        if (!user) {
+            user = await Customer.findById(decodedToken._id).select("-password -refreshToken");
+        }
+
+        req.user = user || null; // Attach user if found, else null
+        next();
+    } catch (error) {
+        authDebug(`Error in optional authentication: ${error.message}`);
+        req.user = null; // Treat as unauthenticated
+        next();
+    }
+});
